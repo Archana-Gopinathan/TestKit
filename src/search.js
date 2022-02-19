27be7c870aa3ea9d6a016703f4b41037a123168b
Archana Gopinathan <Archana.Gopinathan@cognizant.com>
2022-02-19 13:47:02 +0000

    Create a React app and deploy it to Github Page

---
+++ src/search.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import './Search.css';
+import axios from 'axios';
+import PageNavigation from './PageNavigation';
+
+class Search extends React.Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			query: '',
+			results: {},
+			loading: false,
+			message: '',
+			totalResults: 0,
+			totalPages: 0,
+			currentPageNo: 0,
+		};
+
+		this.cancel = '';
+	}
+
+
+	/**
+	 * Get the Total Pages count.
+	 
+	 */
+	getPageCount = ( total, denominator ) => {
+		const divisible	= 0 === total % denominator;
+		const valueToBeAdded = divisible ? 0 : 1;
+		return Math.floor( total/denominator ) + valueToBeAdded;
+	};
+
+	/**
+	 * Fetch the search results and update the state with the result.
+	
+	 */
+	fetchSearchResults = ( updatedPageNo = '', query ) => {
+		const pageNumber = updatedPageNo ? `&page=${updatedPageNo}` : '';
+		const searchUrl = `https://help-search-api-prod.herokuapp.com/search?query=${query}${pageNumber}`;
+
+		if( this.cancel ) {
+			this.cancel.cancel();
+		}
+
+		this.cancel = axios.CancelToken.source();
+
+		axios.get( searchUrl, {
+			cancelToken: this.cancel.token
+		} )
+			.then( res => {
+				const total = res.data.total;
+				const totalPagesCount = this.getPageCount( total, 10 );
+				const resultNotFoundMsg = ! res.data.hits.length
+										? 'No Result.'
+										: '';
+				this.setState( {
+					results: res.data.hits,
+					message: resultNotFoundMsg,
+					totalResults: total,
+					totalPages: totalPagesCount,
+					currentPageNo: updatedPageNo,
+					loading: false
+				} )
+			} )
+			.catch( error => {
+				if ( axios.isCancel(error) || error ) {
+					this.setState({
+						loading: false,
+						message: 'No Data'
+					})
+				}
+			} )
+	};
+
+	handleOnInputChange = ( event ) => {
+		const query = event.target.value;
+		if ( ! query ) {
+			this.setState( { query, results: {}, message: '', totalPages: 0, totalResults: 0 } );
+		} else {
+			this.setState( { query, loading: true, message: '' }, () => {
+				this.fetchSearchResults( 1, query );
+			} );
+		}
+	};
+
+	/**
+	 * Fetch results according to the prev or next page requests.
+	 
+	 */
+	handlePageClick = ( type ) => {
+
+		const updatePageNo = 'prev' === type
+			? this.state.currentPageNo - 1
+			: this.state.currentPageNo + 1;
+
+		if( ! this.state.loading  ) {
+			this.setState( { loading: true, message: '' }, () => {
+				this.fetchSearchResults( updatePageNo, this.state.query );
+			} );
+		}
+	};
+
+	renderSearchResults = () => {
+		const { results } = this.state;
+
+		if ( Object.keys( results ).length && results.length ) {
+			return (
+				<div className="results-container">
+					{ results.map( result => {
+						return (
+							<a key={ result.id } href={ result.previewURL } className="result-item">
+								<h6 className="image-username">{result.user}</h6>
+								<div className="image-wrapper">
+									<img className="image" src={ result.previewURL } alt={`${result.username} image`}/>
+								</div>
+							</a>
+						)
+					} ) }
+
+				</div>
+			)
+		}
+	};
+
+	render() {
+		const { query, loading, message, currentPageNo, totalPages } = this.state;
+
+		const showPrevLink = 1 < currentPageNo;
+		const showNextLink = totalPages > currentPageNo;
+
+		return (
+			<div className="container">
+			{/*	Heading*/}
+			<h2 className="heading">Search Result</h2>
+			{/* Search Input*/}
+			<form className="search-label" htmlFor="search-input">
+				<input
+					type="text"
+					name="query"
+					value={ query }
+					id="search-input"
+					placeholder="Search here"
+					onChange={this.handleOnInputChange}
+				/>
+        <button type="button" class="btn btn-primary"  >Search</button>
+			</form>
+
+			{/*	Error Message*/}
+				{message && <p className="message">{ message }</p>}
+
+			{/*Navigation*/}
+			<PageNavigation
+				loading={loading}
+				showPrevLink={showPrevLink}
+				showNextLink={showNextLink}
+				handlePrevClick={ (event) => this.handlePageClick('prev', event )}
+				handleNextClick={ (event) => this.handlePageClick('next', event )}
+			/>
+
+			{/*	Result*/}
+			{ this.renderSearchResults() }
+
+			{/*Navigation*/}
+			<PageNavigation
+				loading={loading}
+				showPrevLink={showPrevLink}
+				showNextLink={showNextLink}
+				handlePrevClick={ (event) => this.handlePageClick('prev', event )}
+				handleNextClick={ (event) => this.handlePageClick('next', event )}
+			/>
+
+			</div>
+		)
+	}
+}
+
+export default Search
